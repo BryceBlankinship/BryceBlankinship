14718430b1574f7f451f2eaa904dc963601d8b24
Bryce Blankinship <blankinshipbusiness@gmail.com>
2021-11-15 10:24:57 -0500

    everything functional up to this point

--- js/script.js
+++ js/script.js
@@ -13,12 +13,26 @@ let toggleTilt = function () {
 if (location.pathname.match(/fullcpgrid/i))
     setTimeout(toggleTilt, 1000);
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {unlockCard()};
 
-function myFunction() {
-    if (document.documentElement.scrollTop >= 900) {
+function unlockCard() {
+    if (document.documentElement.scrollTop >= 870) {
         body.classList.add('details');
     }else{
         body.classList.remove('details');
     }
 }
+
+let speedlineLink = document.getElementById('speedlineLink');
+
+speedlineLink.addEventListener("mouseenter", function(event) {
+    event.target.style.textShadow = "-2px 2px #242424";
+    event.target.style.marginLeft = "-5px";
+    event.target.style.marginTop = "5px";
+});
+
+speedlineLink.addEventListener("mouseleave", function(event) {
+    event.target.style.textShadow = "-5px 5px #242424";
+    event.target.style.marginLeft = "5px";
+    event.target.style.marginTop = "-5px";
+});
